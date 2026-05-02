7b94a66ea3d424bf7fc0c35dbb0b310c0d80cd10
Katherine Whitlock <kate@skylinesynths.nyc>
2026-05-02 19:32:52 -0400

    Remove rust

--- rust/cmsis-dsp/build.rs
+++
@@ -1,6 +0,0 @@
-//! build.rs for cmsis-dsp
-//!
-//! Nothing to do at the moment; the crate uses cfg(target_arch = "arm")
-//! to select the NEON vs scalar API, which is a built-in Rust cfg.
-
-fn main() {}
